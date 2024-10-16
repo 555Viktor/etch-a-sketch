821c78d738d01ab76663b066c7979ee9179f696c
555Viktor <victor.vaskov2002@gmail.com>
2024-10-16 12:31:22 +0300

    Optimize code structure and add dynamic creation of squares

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-// DOM Elements
+// DOM elements, values and style of sketch container
 let sketchContainer = document.querySelector('.sketch-container');
 
-let gridSize = 16;
-sketchContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-sketchContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+let defaultGridSize = 16;
+sketchContainer.style.gridTemplateRows = `repeat(${defaultGridSize} 1fr)`;
+sketchContainer.style.gridTemplateColumns = `repeat(${defaultGridSize}, 1fr)`;
+
+let inputGridSize = document.querySelector('.input-sketch-size')
+let textGridSize = document.querySelector('.input-sketch-value');
+
+textGridSize.textContent = `${defaultGridSize} x ${defaultGridSize}`;
 
 
+// Function to create grid
 function createGrid (n) {
 
     for (let i = 1; i <= n * n; i++) { 
@@ -17,4 +23,16 @@ function createGrid (n) {
 
 }
 
-createGrid(16)
+// Default 16x16 grid when page is loaded
+createGrid(defaultGridSize)
+
+// Track the value of inputGridSize and dynamically create squares based on input
+inputGridSize.addEventListener('input', () => {
+    let rangeValue = inputGridSize.value;
+
+    sketchContainer.style.gridTemplateRows = `repeat(${rangeValue}, 1fr)`;
+    sketchContainer.style.gridTemplateColumns = `repeat(${rangeValue}, 1fr)`;
+
+    createGrid(rangeValue);
+    textGridSize.textContent = `${rangeValue} x ${rangeValue}`;
+})
